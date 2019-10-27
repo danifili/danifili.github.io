4cb40db35f3bde07d1b38d2ca37dc2352c6859bb
Daniel <danifili96@gmail.com>
2019-10-27 19:10:14 -0400

    Initial commit

---
+++ static/react/precache-manifest.9c14627039e128b7e55f9f1641afe618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5e5494ef3c9495aec8358d0a4dbd2c",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "f6d125c0a67bfa2f113c",
+    "url": "/static/react/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "18fdea7c9aa773b89f59",
+    "url": "/static/react/js/2.2702b553.chunk.js"
+  },
+  {
+    "revision": "f6d125c0a67bfa2f113c",
+    "url": "/static/react/js/main.873da01a.chunk.js"
+  },
+  {
+    "revision": "2740af0358a8b660bf09",
+    "url": "/static/react/js/runtime~main.2052675d.js"
+  }
+]);
